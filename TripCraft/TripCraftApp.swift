d06b60695fd4822b32746892c7a119566c5fb6e2
kai215 <kai215@mds.com.tw>
2025-12-29 09:42:41 +0800

    Initial Commit

---
+++ TripCraft/TripCraftApp.swift
@@ -0,0 +1,17 @@
+//
+//  TripCraftApp.swift
+//  TripCraft
+//
+//  Created by kai on 2025/12/29.
+//
+
+import SwiftUI
+
+@main
+struct TripCraftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
